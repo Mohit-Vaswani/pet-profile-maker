965e0570bc0c086baa35819ac4141ea1a708aae2
Mohit <mohitvaswani010@gmail.com>
2023-10-06 19:22:51 +0530

    some more updates

--- components/PetCard.tsx
+++ components/PetCard.tsx
@@ -1,5 +1,6 @@
 // components/PetCard.tsx
 import React from 'react';
+import { PawPrint } from 'lucide-react';
 
 interface PetCardProps {
   petInfo: {
@@ -16,42 +17,45 @@ interface PetCardProps {
 
 const PetCard: React.FC<PetCardProps> = ({ petInfo }) => {
   return (
-    <div className="petCard w-5/12">
-      <div className="m-10 border-2 rounded-md p-2 border-black w-16">
-        <img src={petInfo.photo} alt={petInfo.petName} className="w-16 h-16 rounded-full" />
-      </div>
-
-      <h2 className="text-xl font-semibold mb-4">Pet Card</h2>
-      <div className="grid grid-cols-2 gap-4">
-        <div className="col-span-2">
-          <label className="block text-sm font-medium text-gray-700">Pet Name:</label>
-          <p className="mt-1 text-gray-900">{petInfo.petName}</p>
+    <div className='w-5/12 petCardContainer flex justify-center items-center'>
+      <div className="m-10 top-0 left-0 border-2 absolute rounded-md p-2 border-black w-16"><PawPrint size={48} /></div>
+    <div className="petCard flex justify-between items-start">
+      <div className="">
+        <div className="">
+          <label className="">Pet Name:</label>
+          <p className="">{petInfo.petName}</p>
         </div>
-        <div className="col-span-2">
-          <label className="block text-sm font-medium text-gray-700">Species:</label>
-          <p className="mt-1 text-gray-900">{petInfo.species}</p>
+        <div className="">
+          <label className="">Species:</label>
+          <p className="">{petInfo.species}</p>
         </div>
-        <div className="col-span-2">
-          <label className="block text-sm font-medium text-gray-700">Breed:</label>
-          <p className="mt-1 text-gray-900">{petInfo.breed}</p>
+        <div className="">
+          <label className="">Breed:</label>
+          <p className="">{petInfo.breed}</p>
         </div>
-        <div className="col-span-2">
-          <label className="block text-sm font-medium text-gray-700">Age:</label>
-          <p className="mt-1 text-gray-900">{petInfo.age}</p>
+        <div className="">
+          <label className="">Age:</label>
+          <p className="">{petInfo.age}</p>
         </div>
-        <div className="col-span-2">
-          <label className="block text-sm font-medium text-gray-700">Gender:</label>
-          <p className="mt-1 text-gray-900">{petInfo.gender}</p>
+        <div className="">
+          <label className="">Gender:</label>
+          <p className="">{petInfo.gender}</p>
         </div>
-        <div className="col-span-2">
-          <label className="block text-sm font-medium text-gray-700">Description:</label>
-          <p className="mt-1 text-gray-900">{petInfo.description}</p>
+        <div className="">
+          <label className="">Description:</label>
+          <p className="">{petInfo.description}</p>
         </div>
-        <div className="col-span-2">
-          <label className="block text-sm font-medium text-gray-700">Owner Name:</label>
-          <p className="mt-1 text-gray-900">{petInfo.ownerName}</p>
+        <div className="">
+          <label className="">Owner Name:</label>
+          <p className="">{petInfo.ownerName}</p>
         </div>
       </div>
+      <div>
+      <div className="border-2 rounded-full object-cover border-black">
+        <img src={petInfo.photo} alt={petInfo.petName} className="w-20 h-20 rounded-full" />
+      </div>
+      </div>
+    </div>
     </div>
   );
 };
